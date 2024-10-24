ef8bb40bfe2564f901f5c5335bfbbb8e3acfecac
ErnestoDanielTafoyaMolinaCYM <e.tafoya@distribuidoracymags.com.mx>
2024-10-24 13:33:10 -0600

    wip show slimes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
+import { Slime } from '../interfaces/slime.interface';
 
 @Component({
   selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  templateUrl: './tab1.page.html',
+  styleUrls: ['./tab1.page.scss'],
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  slimes: Slime[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient, private alertController: AlertController) {}
 
+  ngOnInit() {
+    this.getSlimes();
+  }
+
+  getSlimes() {
+    this.http.get<{ status: string; slimes: Slime[] }>('https://slime-rancher.vercel.app/api/slime')
+      .subscribe(
+        (data) => {
+          console.log("DATOS: ",data);
+          if (data.status === 'success') {
+            this.slimes = data.slimes;
+          }
+        },
+        (error) => {
+          console.error('Error fetching slimes:', error);
+        }
+      );
+  }
+
+  async showSlimeInfo(slime: Slime) {
+    const alert = await this.alertController.create({
+      header: slime.name,
+      subHeader: slime.type,
+      message: `Diet: ${slime.diet}<br>Favourite Food: ${slime.favouriteFood}<br>Slimeology: ${slime.slimepedia.slimeology}`,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 }
